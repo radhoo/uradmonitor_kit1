81a3b88b190319ae9a7ca80ff09595476c9b5f63
radhoo <radhoo.tech@gmail.com>
2018-04-12 13:41:09 +0300

    added SBT10A

--- code/geiger/detectors.h
+++ code/geiger/detectors.h
@@ -28,18 +28,20 @@
 #include <stdint.h>
 
 // define Radiation DETECTORS
-#define GEIGER_TUBE_UNKNOWN 0x0
-#define GEIGER_TUBE_SBM20 0x1
-#define GEIGER_TUBE_SI29BG 0x2
-#define GEIGER_TUBE_SBM19 0x3
-#define GEIGER_TUBE_LND712 0x4
-#define GEIGER_TUBE_SBM20M 0x5
-#define GEIGER_TUBE_SI22G 0x6
-#define GEIGER_TUBE_STS5 0x7
-#define GEIGER_TUBE_SI3BG 0x8
-#define GEIGER_TUBE_SBM21 0x9
-#define GEIGER_TUBE_SBT9 0xA
-#define GEIGER_TUBE_SI1G 0xB
+#define GEIGER_TUBE_UNKNOWN 	0x0
+#define GEIGER_TUBE_SBM20 		0x1
+#define GEIGER_TUBE_SI29BG 		0x2
+#define GEIGER_TUBE_SBM19 		0x3
+#define GEIGER_TUBE_LND712 		0x4
+#define GEIGER_TUBE_SBM20M 		0x5
+#define GEIGER_TUBE_SI22G 		0x6
+#define GEIGER_TUBE_STS5 		0x7
+#define GEIGER_TUBE_SI3BG 		0x8
+#define GEIGER_TUBE_SBM21 		0x9
+#define GEIGER_TUBE_SBT9 		0xA
+#define GEIGER_TUBE_SI1G 		0xB
+#define GEIGER_TUBE_SI8B 		0xC
+#define GEIGER_TUBE_SBT10A 		0xD
 
 
 char *aux_detectorName(uint8_t param);
